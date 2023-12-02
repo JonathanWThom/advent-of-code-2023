2914f20d3664ef3dad9773b56fe65291aafecdd5
Jonathan Thom <jonathanthom@hey.com>
2023-12-02 14:36:09 -0800

    Day 2 part 2

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -5,7 +5,35 @@ module Day2
     end
 
     def self.part_2(path)
-      0
+      SnowIslandWater.new(path).fewest_cubes_power
+    end
+  end
+
+  module HasGames
+    # todo
+  end
+
+  class SnowIslandWater
+    def initialize(path)
+      @path = path
+    end
+
+    def fewest_cubes_power
+      game_powers.sum
+    end
+
+    private
+
+    def lines
+      @_lines ||= File.readlines(@path)
+    end
+
+    def game_powers
+      games.map(&:cube_power)
+    end
+
+    def games
+      @_games ||= lines.map { |line| Game.new(line) }
     end
   end
 
@@ -46,6 +74,28 @@ module Day2
       draws.any? { |draw| draw.impossible? }
     end
 
+    def cube_power
+      max_red = 0
+      max_green = 0
+      max_blue = 0
+
+      draws.each do |draw|
+        if draw.max_red > max_red
+          max_red = draw.max_red
+        end
+
+        if draw.max_green > max_green
+          max_green = draw.max_green
+        end
+
+        if draw.max_blue > max_blue
+          max_blue = draw.max_blue
+        end
+      end
+
+      max_red * max_green * max_blue
+    end
+
     private
 
     attr_reader :line
@@ -68,6 +118,18 @@ module Day2
       red_over? || blue_over? || green_over?
     end
 
+    def max_red
+      all_red.max || 0
+    end
+
+    def max_green
+      all_green.max || 0
+    end
+
+    def max_blue
+      all_blue.max || 0
+    end
+
     private
 
     attr_reader :draw
@@ -76,10 +138,20 @@ module Day2
       @_cubes ||= @draw.split(", ")
     end
 
+    def all_red
+      @_all_red ||= all_of_color("red")
+    end
+
+    def all_green
+      @_all_green ||= all_of_color("green")
+    end
+
+    def all_blue
+      @_all_blue ||= all_of_color("blue")
+    end
+
     def red
-      cubes.detect do |cube|
-        /red/.match(cube)
-      end&.scan(/\d+/)&.first&.to_i || 0
+      first_of_color("red")
     end
 
     def red_over?
@@ -87,9 +159,7 @@ module Day2
     end
 
     def blue
-      cubes.detect do |cube|
-        /blue/.match(cube)
-      end&.scan(/\d+/)&.first&.to_i || 0
+      first_of_color("blue")
     end
 
     def blue_over?
@@ -97,13 +167,25 @@ module Day2
     end
 
     def green
-      cubes.detect do |cube|
-        /green/.match(cube)
-      end&.scan(/\d+/)&.first&.to_i || 0
+      first_of_color("green")
     end
 
     def green_over?
       green > GREEN_LIMIT
+    end
+
+    def all_of_color(color)
+      cubes.select do |cube|
+        Regexp.new(color).match(cube)
+      end.map do |cube|
+        cube.scan(/\d+/).first.to_i
+      end
+    end
+
+    def first_of_color(color)
+      cubes.detect do |cube|
+        Regexp.new(color).match(cube)
+      end&.scan(/\d+/)&.first&.to_i || 0
     end
   end
 end
--- day_2/solution_test.rb
+++ day_2/solution_test.rb
@@ -7,14 +7,14 @@ class Day2::SolutionTest < Minitest::Test
   end
 
   def test_part_1_full
-    assert_equal(54304, Day2::Solution.part_1("day_2/input.txt"))
+    assert_equal(2377, Day2::Solution.part_1("day_2/input.txt"))
   end
 
-  #def test_part_2_sample
-    #assert_equal(281, Day2::Solution.part_2("day_2/input_sample.txt"))
-  #end
+  def test_part_2_sample
+    assert_equal(2286, Day2::Solution.part_2("day_2/input_sample.txt"))
+  end
 
-  #def test_part_2_full
-    #assert_equal(54418, Day2::Solution.part_2("day_2/input.txt"))
-  #end
+  def test_part_2_full
+    assert_equal(71220, Day2::Solution.part_2("day_2/input.txt"))
+  end
 end
